0d2f85ea9b2558493d73d0f5416296829978b44e
tomboyloychoy <89024064+tomboyloychoy@users.noreply.github.com>
2022-02-28 23:39:40 -0500

    Add files via upload

---
+++ HIITFit/HIITFit/Model/HistoryStore.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct ExerciseDay: Identifiable {
+  let id = UUID()
+  let date: Date
+  var exercises: [String] = []
+}
+
+class HistoryStore: ObservableObject {
+  @Published var exerciseDays: [ExerciseDay] = []
+
+  init() {
+    #if DEBUG
+    createDevData()
+    #endif
+  }
+
+  func addDoneExercise(_ exerciseName: String) {
+    let today = Date()
+    if today.isSameDay(as: exerciseDays[0].date) {
+      print("Adding \(exerciseName)")
+      exerciseDays[0].exercises.append(exerciseName)
+    } else {
+      exerciseDays.insert(
+        ExerciseDay(date: today, exercises: [exerciseName]),
+        at: 0)
+    }
+  }
+}
---
+++ HIITFit/HIITFit/Views/RatingView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct RatingView: View {
+  @Binding var rating: Int
+  let maximumRating = 5
+
+  let onColor = Color.red
+  let offColor = Color.gray
+
+  var body: some View {
+    HStack {
+      ForEach(1 ..< maximumRating + 1) { index in
+        Image(systemName: "waveform.path.ecg")
+          .foregroundColor(
+            index > rating ? offColor : onColor)
+          .onTapGesture {
+            rating = index
+          }
+      }
+    }
+    .font(.largeTitle)
+  }
+}
+
+struct RatingView_Previews: PreviewProvider {
+  static var previews: some View {
+    RatingView(rating: .constant(3))
+      .previewLayout(.sizeThatFits)
+  }
+}
